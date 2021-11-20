30254ba2234e32eb5a1fa2f11c424007d1a23f6e
mohammadekhosravi <mohammadekhosravi@yahoo.com>
2021-11-20 16:42:22 +0330

    Refactor resultsView and bookmarksView to use composition with previewView and generate markup without duplication

--- src/js/views/bookmarksView.js
+++ src/js/views/bookmarksView.js
@@ -1,33 +1,15 @@
 import View from './View.js';
-
-// Import svg icons
-import icons from '../../img/icons.svg';
+import previewView from './previewView.js';
 
 class BookmarksView extends View {
   _parentElement = document.querySelector('.bookmarks__list');
-  _errorMessage = 'No bookmarks yet. Find a nice recipe and bookmark it';
+  _errorMessage = 'No bookmarks yet. Find a nice recipe and bookmark it ;)';
   _message = '';
 
   _generateMarkup() {
-    return this._data.map(result => this._generateMarkupPreview(result)).join('');
-  }
-
-  _generateMarkupPreview(result) {
-    const id = window.location.hash.slice(1);
-
-    return `
-      <li class="preview">
-        <a class="preview__link ${id === result.id ? "preview__link--active" : ''}" href="#${result.id}">
-          <figure class="preview__fig">
-            <img src="${result.image}" alt="${result.title}" />
-          </figure>
-          <div class="preview__data">
-            <h4 class="preview__title">${result.title}</h4>
-            <p class="preview__publisher">${result.publisher}</p>
-          </div>
-        </a>
-      </li>
-    `;
+    return this._data
+      .map(bookmark => previewView.render(bookmark, false))
+      .join('');
   }
 }
 
--- src/js/views/resultsView.js
+++ src/js/views/resultsView.js
@@ -1,7 +1,5 @@
 import View from './View.js';
-
-// Import svg icons
-import icons from '../../img/icons.svg';
+import previewView from './previewView.js';
 
 class ResultsView extends View {
   _parentElement = document.querySelector('.results');
@@ -9,25 +7,9 @@ class ResultsView extends View {
   _message = '';
 
   _generateMarkup() {
-    return this._data.map(result => this._generateMarkupPreview(result)).join('');
-  }
-
-  _generateMarkupPreview(result) {
-    const id = window.location.hash.slice(1);
-
-    return `
-      <li class="preview">
-        <a class="preview__link ${id === result.id ? "preview__link--active" : ''}" href="#${result.id}">
-          <figure class="preview__fig">
-            <img src="${result.image}" alt="${result.title}" />
-          </figure>
-          <div class="preview__data">
-            <h4 class="preview__title">${result.title}</h4>
-            <p class="preview__publisher">${result.publisher}</p>
-          </div>
-        </a>
-      </li>
-    `;
+    return this._data
+      .map(result => previewView.render(result, false))
+      .join('');
   }
 }
 
